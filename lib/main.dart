03fefcbc8314675e5729d06fda6808d66b79f0ed
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-03-26 00:19:57 +0200

    initial project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+    );
+  }
+}
