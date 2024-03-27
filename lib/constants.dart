e91ad6642c01824ce2c5f722eb08468d41249d42
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-03-27 02:09:11 +0200

    Create Styles

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,3 +4,5 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xff100b20);
 
 const kTransitionDuration = Duration(milliseconds: 250);
+
+const kGtSectraFine = 'Gt Sectra Fine';
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 import 'custom_app_bar.dart';
@@ -15,8 +16,48 @@ class HomeViewBody extends StatelessWidget {
         children: [
           CustomAppBar(),
           FeaturedBooksListView(),
-          SizedBox(height: 50,),
-          Text('Best Seller', style: Styles.titleMedium,),
+          SizedBox(
+            height: 50,
+          ),
+          Text(
+            'Best Seller',
+            style: Styles.textStyle18,
+          ),
+          BestSellerListViewItem(),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              height: 200,
+              width: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red,
+                image: const DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(
+                      AssetsData.testImage,
+                    )),
+              ),
+            ),
+          ),
+          Column(
+            children: [],
+          )
         ],
       ),
     );
