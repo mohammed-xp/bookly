74d51b9ad1531b69972787bcec684a5ef39ce5f4
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-03-29 14:43:31 +0200

    Create Featured Books cubit states and methods

---
+++ lib/features/home/presentation/manager/featured_book_cubit/featured_books_state.dart
@@ -0,0 +1,17 @@
+part of 'featured_books_cubit.dart';
+
+@immutable
+sealed class FeaturedBooksState {}
+
+final class FeaturedBooksInitial extends FeaturedBooksState {}
+final class FeaturedBooksLoading extends FeaturedBooksState {}
+final class FeaturedBooksSuccess extends FeaturedBooksState {
+  final List<BookModel> books;
+
+  FeaturedBooksSuccess(this.books);
+}
+final class FeaturedBooksFailure extends FeaturedBooksState {
+  final String errMessage;
+
+  FeaturedBooksFailure(this.errMessage);
+}
