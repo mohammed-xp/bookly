d66bfa6fa235e7da76e4bce82b7060a6b6efc247
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-03-29 11:34:42 +0200

    Done fetchFeaturedBooks method in HomeRepoIml

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -11,9 +11,26 @@ class HomeRepoImpl extends HomeRepo {
   HomeRepoImpl(this.apiService);
 
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    apiService.get(endPoint: '');
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async{
+    try {
+      var data = await apiService.get(
+        endPoint:
+        'volumes?Filtering=free-ebooks&q=subject:Programming',
+      );
+
+      List<BookModel> books = [];
+      for(var item in data['items']){
+        books.add(BookModel.fromJson(item));
+      }
+
+      return right(books);
+    } catch (e) {
+      /// Change DioError to DioException
+      if(e is DioError){
+        return left(ServerFailure.fromDioError(e));
+      }
+      return left(ServerFailure(e.toString()));
+    }
   }
 
   @override
