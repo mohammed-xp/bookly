1c686e1d61ff6d39e1ff169d6c68cbf963c42781
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-03-29 21:29:27 +0200

    Add get_it (setup service locator)

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -2,15 +2,15 @@ import 'package:dio/dio.dart';
 
 class ApiService{
 
-  final Dio _dio;
-  ApiService(this._dio);
+  final Dio dio;
+  ApiService(this.dio);
 
   final String _baseUrl = 'https://www.googleapis.com/books/v1/';
 
   Future<Map<String, dynamic>> get({
     required String endPoint,
 }) async{
-    var response = await _dio.get('$_baseUrl$endPoint');
+    var response = await dio.get('$_baseUrl$endPoint');
     return response.data;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:bookly/core/utils/api_service.dart';
 import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/service_locator.dart';
+import 'package:bookly/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly/features/home/presentation/manager/featured_book_cubit/featured_books_cubit.dart';
+import 'package:bookly/features/home/presentation/manager/news_books_cubit/news_books_cubit.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'constants.dart';
 
 void main() {
+  setupServiceLocator();
   runApp(const Bookly());
 }
 
@@ -13,15 +21,30 @@ class Bookly extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      title: 'Bookly App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => NewsBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        title: 'Bookly App',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme:
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
+        // home: const SplashView(),
       ),
-      // home: const SplashView(),
     );
   }
 }
