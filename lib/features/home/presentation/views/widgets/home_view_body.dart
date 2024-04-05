3bc37f3f84ab48400b9a1727125cdec4e0c68b47
Mohammed Adil <66908713+mohammed-xp@users.noreply.github.com>
2024-04-05 02:39:14 +0200

    Add pull to refresh in home view

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,8 @@
 import 'package:bookly/core/utils/styles.dart';
+import 'package:bookly/features/home/presentation/manager/featured_book_cubit/featured_books_cubit.dart';
+import 'package:bookly/features/home/presentation/manager/news_books_cubit/news_books_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'news_books_list_view.dart';
 import 'custom_app_bar.dart';
 import 'featured_books_list_view.dart';
@@ -9,42 +12,48 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView(
-      // shrinkWrap: true,
-      physics: BouncingScrollPhysics(),
-      slivers: [
-        SliverToBoxAdapter(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24),
-                child: CustomAppBar(),
-              ),
-              FeaturedBooksListView(),
-              SizedBox(
-                height: 50,
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 24),
-                child: Text(
-                  'Newest Books',
-                  style: Styles.textStyle18,
+    return RefreshIndicator(
+      onRefresh: () async {
+        BlocProvider.of<FeaturedBooksCubit>(context).fetchFeaturedBooks();
+        BlocProvider.of<NewsBooksCubit>(context).fetchNewsBooks();
+      },
+      child: const CustomScrollView(
+        // shrinkWrap: true,
+        physics: BouncingScrollPhysics(),
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24),
+                  child: CustomAppBar(),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-            ],
+                FeaturedBooksListView(),
+                SizedBox(
+                  height: 50,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 24),
+                  child: Text(
+                    'Newest Books',
+                    style: Styles.textStyle18,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+              ],
+            ),
           ),
-        ),
-        SliverToBoxAdapter(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24),
-            child: NewsBooksListView(),
-          ),
-        )
-      ],
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: NewsBooksListView(),
+            ),
+          )
+        ],
+      ),
     );
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 30),
